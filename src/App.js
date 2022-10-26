e63482d6003541d4b5d85a0e979b93be1465f6fa
Linh Nguyen <linh.nguyen@everfit.io>
2022-10-26 20:41:26 +0700

    Linh has created a new commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hoc reactjs
+          Edit <code>src/App.js</code> Hoc reactjs - Linh has updated this line
         </p>
         <a
           className="App-link"
